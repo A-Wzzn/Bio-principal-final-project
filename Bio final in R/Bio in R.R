2aa8a6a0aaf4c212c44df81026447365fd8c5af7
A-Wzzn <154960978+A-Wzzn@users.noreply.github.com>
2023-12-27 07:21:16 +0000

    Update Bio in R.R

--- Bio final in R/Bio in R.R
+++ Bio final in R/Bio in R.R
@@ -8,7 +8,7 @@ file_name = "brca_tcga_pan_can_atlas_2018.tar.gz"
 untar(file_name)
 
 # change directory to the extracted folders 
-##setwd(paste(getwd() , "/brca_tcga_pan_can_atlas_2018", sep = ""))
+setwd(paste(getwd() , "/brca_tcga_pan_can_atlas_2018", sep = ""))
 
 # data_clinical_patient.txt, data_mrna_seq_v2_rsem.txt, data_mutations.txt and data_cna.txt
 clinical = read.delim("data_clinical_patient.txt")
